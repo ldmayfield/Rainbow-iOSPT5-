f78477e15904e09d230547b2f06f06d6f785d2e5
ldmayfield <ldonnamayfield@gmail.com>
2020-01-28 21:04:03 -0700

    updated colors labels and backgrounds

---
+++ Rainbow (iOSPT5)/Model/Color.swift
@@ -0,0 +1,15 @@
+//
+//  Color.swift
+//  Rainbow (iOSPT5)
+//
+//  Created by Lambda_School_Loaner_246 on 1/28/20.
+//  Copyright © 2020 Lambda_School_Loaner_246. All rights reserved.
+//
+
+import UIKit
+
+struct MyColor {
+    let name: String
+    let color: UIColor
+}
+
---
+++ Rainbow (iOSPT5)/View Controllers/ColorDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ColorDetailViewController.swift
+//  Rainbow (iOSPT5)
+//
+//  Created by Lambda_School_Loaner_246 on 1/28/20.
+//  Copyright © 2020 Lambda_School_Loaner_246. All rights reserved.
+//
+
+import UIKit
+
+class ColorDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
